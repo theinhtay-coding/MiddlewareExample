f8bb816f3fcb6540126a6826b40da177c39e71d3
theinhtay <theinhtay@aceplussolutions.com>
2025-09-03 11:42:44 +0630

    Add project files.

---
+++ MiddlewareExample/CustomMiddleware/MyCustomMiddleware.cs
@@ -0,0 +1,13 @@
+﻿
+namespace MiddlewareExample.CustomMiddleware
+{
+    public class MyCustomMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            await context.Response.WriteAsync("My custom middleware - start");
+            await next(context);
+            await context.Response.WriteAsync("My custom middleware - end");
+        }
+    }
+}
---
+++ MiddlewareExample/Program.cs
@@ -0,0 +1,31 @@
+using MiddlewareExample.CustomMiddleware;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddTransient<MyCustomMiddleware>();
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+//mideleware 1
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+    await context.Response.WriteAsync("From Middleware 1");
+    await next(context);
+});
+
+////middleware 2
+//app.Use(async (HttpContext context,RequestDelegate next) =>
+//{
+//    await context.Response.WriteAsync("Hello again");
+//    await next(context);
+//}); 
+
+app.UseMiddleware<MyCustomMiddleware>();
+
+//middleware 3
+app.Run(async (HttpContext context) =>
+{
+    await context.Response.WriteAsync("From Middleware 3");
+});
+
+app.Run();
